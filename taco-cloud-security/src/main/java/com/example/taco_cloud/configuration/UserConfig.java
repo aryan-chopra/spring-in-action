c99f0134a9b69dea97680c440b3fc9758181b988
aryan-chopra <aryan.chopra1211@gmail.com>
2025-07-29 11:25:15 +0530

    Added a comment for clarity purposes

--- taco-cloud-security/src/main/java/com/example/taco_cloud/configuration/UserConfig.java
+++ taco-cloud-security/src/main/java/com/example/taco_cloud/configuration/UserConfig.java
@@ -10,6 +10,13 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 @Configuration
 public class UserConfig {
 
+    /**
+     * This method builds an implementation of the "UserDetailsService" interface
+     * used to load a User by their username
+     * @param userRepository the userRepository used to interact with Database
+     * @return UserDetailsService the implemented interface used to fetch a user from DB.
+     */
+
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepository) {
         return username -> {
